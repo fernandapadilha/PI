937a98fac3410c59ceb3f14a75433267ab811742
CaetanoPF <caetanofreitas44@gmail.com>
2023-09-26 15:16:51 -0300

    up

--- java/script.js
+++
@@ -1,9 +0,0 @@
-function menuAbre() {
-    let menu = document.querySelector('.mobile-menu');
-    if (menu.classList.contains('open')) {
-        menu.classList.remove('open');
-    }
-    else {
-        menu.classList.add('open');
-    }
-}
